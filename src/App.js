a86da6df7b4b5b239314cfd66fe9c233420e0862
ModWrk <erik@modwrk.com>
2018-09-03 12:04:33 -0600

    add linter.json

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie.js';
+
+
+const movies =[{
+    id: 1,
+    title: 'Star Wars',
+    desc: 'A friggin movie about space, n yetis, n aliens, n stuff'
+  }, {
+    id:2,
+    title: 'Usual Suspects',
+    desc: 'Damn it, Kevin Spacey! We loved you! :('
+  }, {
+    id: 3,
+    title: 'The Red Cliff',
+    desc: 'A Kung Fu epic based on a historical legend.'
+  }
+];
+
 class App extends Component {
+  async componentDidMount() {
+    try{
+      const res = await fetch();
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      })
+
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc}/>)}
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Movie = (props) => (
+  <div>
+    <h3>{props.movie.title}</h3>
+  </div>
+);
+
+
+export default Movie;
+
+
+
+// static propTypes = {
+//   movie: PropTypes.shape({
+//     title: PropTypes.string.isRequired
+//   }),
+// }
